5055d2a4b7e5a8de49dd9545166f5344dec50813
Andrew Backes <backes.andrew@gmail.com>
2013-02-26 11:07:32 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shopoo::Application.config.secret_token = '490aed672c86e75727215e045b60a02146e40c9bcc3599dcf83076125fd0df1f9d26100ece069382a8d4dd41556caab399411a55ccf7842e5d983dd43e5c28a1'
