d258bdd35dd087398110f39baef607b21370070c
Andrew Backes <backes.andrew@gmail.com>
2013-03-06 20:52:59 -0600

    Links added to My Account and Edit Profile

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130305040910) do
+ActiveRecord::Schema.define(:version => 20130302231112) do
 
   create_table "addresses", :force => true do |t|
     t.string   "streetaddress"
@@ -78,15 +78,15 @@ ActiveRecord::Schema.define(:version => 20130305040910) do
   create_table "users", :force => true do |t|
     t.string   "firstname"
     t.string   "lastname"
-    t.string   "password"
+    t.string   "encrypted_password"
+    t.string   "salt"
     t.string   "email"
     t.integer  "billing_info_id"
     t.integer  "address_id"
     t.integer  "cart_id"
     t.boolean  "admin"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
-    t.string   "salt"
+    t.datetime "created_at",         :null => false
+    t.datetime "updated_at",         :null => false
   end
 
 end
