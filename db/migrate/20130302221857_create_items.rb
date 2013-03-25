295addbf73d699a932d6607b88ba6cff9788d54f
Andrew Backes <backes.andrew@gmail.com>
2013-03-25 16:15:23 -0500

    Edit create_items migration

--- db/migrate/20130302221857_create_items.rb
+++ db/migrate/20130302221857_create_items.rb
@@ -2,7 +2,7 @@ class CreateItems < ActiveRecord::Migration
   def change
     create_table :items do |t|
       t.string :name
-      t.decimal :cost, :scale => 2
+      t.decimal :cost
       t.string :description
       t.integer :stock
       t.string :url
